28d1f846fcbd2b567eab4e188630f1eb06cc8926
MarcioKruger <ms.kruger@hotmail.com>
2023-09-29 15:41:44 -0300

    .

--- static/script.js
+++ static/script.js
@@ -1,11 +1,23 @@
 /* Abre e fecha menu lateral em modo mobile */
 
-const menuBobile = document.querySelector('.menu-mobile')
+const menuMobile = document.querySelector('.menu-mobile')
 const body = document.querySelector('body')
 
-menuBobile.addEventListener('click', ()=>{
-    menuBobile.classList.contains("bi-list")
-    ? menuBobile.classList.replace("bi-list", "bi-x")
-    : menuBobile.classList.replace("bi-x", "bi-list");
+menuMobile.addEventListener('click', ()=>{
+    menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list", "bi-x")
+    : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle("menu-nav-active");
+})
+
+/* Fecha o menu quando clicar em algum item e muda o icone para list*/
+
+const navItem=document.querySelectorAll('.nav-item')
+navItem.forEach(item =>{
+    item.addEventListener("click", ()=>{
+        if(body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
 })
